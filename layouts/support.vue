947da858ab334efcd501fd4a4cb56881e40b329c
Renaud Charpentier <renaud.charpentier@oclock.io>
2022-06-15 16:06:30 +0200

    Mise en place de Nuxt.js + début du support front-back

---
+++ layouts/support.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-app dark>
+    <v-navigation-drawer
+      v-model="drawer"
+      mini-variant
+      clipped
+      expand-on-hover
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main>
+      <v-container fluid>
+        <Nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'SupportLayout',
+  data () {
+    return {
+      title: "O'clock supports",
+      drawer: true,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Client / Serveur',
+          to: '/front-back'
+        },
+      ],
+    }
+  }
+}
+</script>
