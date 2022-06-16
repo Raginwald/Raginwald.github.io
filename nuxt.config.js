cfed13edde1dd299d6e665490a7384ef4ebd0935
Renaud Charpentier <renaud.charpentier@oclock.io>
2022-06-16 08:54:35 +0200

    Test

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,9 +7,9 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
-  // router: {
-  //   base: '/raginwald.github.io/'
-  // },
+  router: {
+    base: '/dist/'
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
